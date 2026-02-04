30361ae51b3753b891a0cc0f221af2759be30a58
Liu Haozhe <lhz7269874@outlook.com>
2026-02-04 21:59:45 +0800

    add initial files

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::cout << "Hello, CMake and C++!" << std::endl;
+    
+    // 稍微用点C++特性，证明配置成功
+    std::vector<int> v = {1, 2, 3};
+    return 0;
+}
